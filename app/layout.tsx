7160e3395821cac8a95ddc11a0f3fb7e369c360a
Alex Sherman <asherman.ca@gmail.com>
2023-07-13 10:02:48 -0700

    bug - pnpm deploy fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Nav from '../components/Nav'
 import Providers from '@/components/Providers'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -25,6 +26,7 @@ export default function RootLayout({
 				<Providers>
 					<Nav />
 					{children}
+					<Analytics />
 				</Providers>
 			</body>
 		</html>
