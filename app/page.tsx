b561e11a3ac3c3e8bf29da42af9a57cb17512643
Alex Sherman <asherman.ca@gmail.com>
2023-07-13 09:07:14 -0700

    page - homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,26 @@
 import Image from 'next/image'
-import dashboardpreview from '../public/dashboardpreview.jpg'
+import dashboardpreviewcrop from '../public/dashboardpreviewcrop.jpg'
 
 export default function Home() {
 	return (
-		<main className='flex-1 flex flex-col p-4 gap-8'>
-			<div className='flex flex-col gap-4 mt-4'>
+		<main className='flex-1 flex justify-center items-center p-4 gap-8 relative'>
+			<div className='absolute top-0 right-0 -z-10 opacity-25'>
+				<Image
+					src={dashboardpreviewcrop}
+					alt='dashboard preview'
+					className='border border-gray-200'
+				/>
+			</div>
+			<div className='flex flex-col  gap-4 mt-4 bg-white p-8 border-2 border-gray-200 max-w-[50%] rounded-md shadow-md'>
 				<h1 className='font-bold text-5xl'>Organize your Job Search</h1>
 				<h2 className='font-semibold text-lg'>Optimized CVs with Openai</h2>
-				<p className='font-semibold text-lg max-w-[50%]'>
+				<p className='font-semibold text-lg'>
 					Save time and use AI to write coverletters from your laptop or phone
 					instead of wasting countless hours. Organize your job searches and
 					applications with our streamlined dashboard.
 				</p>
 			</div>
-			<div className='flex justify-between gap-4 max-w-fit'>
+			{/* <div className='flex justify-between gap-4 max-w-fit'>
 				<div>
 					<h3 className='font-semibold text-xl mb-2'>Dashboard</h3>
 					<Image
@@ -30,7 +37,7 @@ export default function Home() {
 						className='border border-gray-200 rounded-md'
 					/>
 				</div>
-			</div>
+			</div> */}
 		</main>
 	)
 }
